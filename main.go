c2bb53a54a9460df62ad437e3e2569dc86cf43c0
gruz123 <32638741+gruz123@users.noreply.github.com>
2022-04-27 14:23:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,23 +16,10 @@ func HandlerRequest() {
 	var dir string
 	//http.FileServer(http.Dir("./web/assets/"))
 	r := mux.NewRouter()
-
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
 	r.Use(cors)
-
-	//r.HandleFunc("/list", Project_list).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/addProject", AddProject).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/addlistcode", AddListCode).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/listcode", ListCode).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/getdatacode", GetData).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/pushparamcode", Pushdate).Methods("POST", "OPTIONS")
-
 	r.HandleFunc("/addFile", AddFile).Methods("POST", "OPTIONS")
 	r.HandleFunc("/download", GetImage).Methods(http.MethodGet, "OPTIONS")
-	//r.HandleFunc("/deleteCode", DeleteCode).Methods("POST", "OPTIONS")
-	////r.HandleFunc("/chaingeName", ChaingeName).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/deleteApp", DeleteApp).Methods("POST", "OPTIONS")
-	//r.HandleFunc("/getName", GetName).Methods("POST", "OPTIONS")
 
 	log.Fatal(http.ListenAndServe(":10002", r))
 }
